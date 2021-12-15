d1bc0e873b6f9c8769c20e0b3560f4e2f704b71c
Anshul <anshulmax1@gmail.com>
2021-12-14 18:03:13 -0600

    final

--- files.py
+++ files.py
@@ -1,9 +1,8 @@
-#import pandas as pd
-housingFile = "/Users/anshul/Desktop/Principles\ of\ Programming/HousingProject-master/files/housing-info.csv"
-incomeFile = "/Users/anshul/Desktop/Principles\ of\ Programming/HousingProject-master/files/income-info.csv"
-zipFile = "Users/anshul/Desktop/Principles\ of\ Programming/HousingProject-master/files/zip-city-county-state.csv"
+housingFile = "/Users/anshul/Desktop/Principles of Programming/HousingProject-master/files/housing-info.csv"
+incomeFile = "/Users/anshul/Desktop/Principles of Programming/HousingProject-master/files/income-info.csv"
+zipFile = "/Users/anshul/Desktop/Principles of Programming/HousingProject-master/files/zip-city-county-state.csv"
 
 username = 'root'
-password = 'insertyourshere'
+password = 'insertyours'
 hostname = 'localhost'
 database = 'housing_project'
--- main.py
+++ main.py
@@ -7,58 +7,47 @@ import numpy as np
 import re
 import pymysql.cursors
 from files import *
-
-GuidList = []
-
+Guid_List = []
 # read in records into three data frames
 housingDataFrame = pd.read_csv(housingFile)
 incomeDataFrame = pd.read_csv(incomeFile)
 zipDataFrame = pd.read_csv(zipFile)
-
-# join data based on guid
+# combine databases on guid
 HousingAndIncome = housingDataFrame.join(incomeDataFrame.set_index('guid'),
                                          on='guid',
                                          lsuffix='_housing',
                                          rsuffix='_income')
-CombinedDataFrame = HousingAndIncome.join(zipDataFrame.set_index('guid'),
-                                          on='guid')
-CombinedDataFrame = CombinedDataFrame.drop(columns=['zip_code_housing', 'zip_code_income'])
-CombinedDataFrame = CombinedDataFrame.rename(columns={'housing_median_age': 'median_age'})
-
-# stripping hyphens from guid so that it will properly read into the database
-for guid, row in CombinedDataFrame.iterrows():
+Combined_Data_Frame = HousingAndIncome.join(zipDataFrame.set_index('guid'),
+                                            on='guid')
+Combined_Data_Frame = Combined_Data_Frame.drop(columns=['zip_code_housing', 'zip_code_income'])
+Combined_Data_Frame = Combined_Data_Frame.rename(columns={'housing_median_age': 'median_age'})
+# remove hyphens to allow it to be the right size for it to work.
+for guid, row in Combined_Data_Frame.iterrows():
     stripped = row['guid'].replace('-', "")
-    GuidList.append(stripped)
-
-GuidDf = pd.DataFrame(data=GuidList, columns=['guid'])
-CombinedDataFrame.update(GuidDf)
-
+    Guid_List.append(stripped)
+GuidDf = pd.DataFrame(data=Guid_List, columns=['guid'])
+Combined_Data_Frame.update(GuidDf)
 # cleaning data
 # dropping corrupt guid
-for guid, row in CombinedDataFrame.iterrows():
+for guid, row in Combined_Data_Frame.iterrows():
     check = re.fullmatch("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['guid'])
-
     if check is not None:
         CorruptGuid = row['guid']
-        CombinedDataFrame.replace(CorruptGuid, np.nan, True)
-        CombinedDataFrame.dropna(inplace=True)
-
+        Combined_Data_Frame.replace(CorruptGuid, np.nan, True)
+        Combined_Data_Frame.dropna(inplace=True)
     else:
         pass
-
 # assigning corrupt zip code with the beginning number followed by 0000 of good zip code of the same state
-for code, row in CombinedDataFrame.iterrows():
+for code, row in Combined_Data_Frame.iterrows():
     check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['zip_code'])
-
     if check is not None:
         CorruptRecord = row['guid']
         CorruptZip = row['zip_code']
         City = row['city']
         State = row['state']
-
-        for CityState, record in CombinedDataFrame.iterrows():
-            goodCheck = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", record['zip_code'])
-            if goodCheck is None:
+        for CityState, record in Combined_Data_Frame.iterrows():
+            valid_check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", record['zip_code'])
+            if valid_check is None:
                 if CorruptRecord != record['guid']:
                     if State == record['state']:
                         UseZip = record['zip_code']
@@ -70,83 +59,66 @@ for code, row in CombinedDataFrame.iterrows():
                         pass
                 else:
                     pass
-        CombinedDataFrame.replace(CorruptZip, zipPadded, True)
+        Combined_Data_Frame.replace(CorruptZip, zipPadded, True)
     else:
         pass
-
 # replace corrupt age, rooms, bedrooms, population, households, value, & income with random number between certain range
-for age, row in CombinedDataFrame.iterrows():
+for age, row in Combined_Data_Frame.iterrows():
     check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['median_age'])
-
     if check is not None:
         CorruptAge = row['median_age']
         randAge = np.random.randint(10, 50)
-        CombinedDataFrame.replace(CorruptAge, randAge, True)
+        Combined_Data_Frame.replace(CorruptAge, randAge, True)
     else:
         pass
-
-
-for rooms, row in CombinedDataFrame.iterrows():
+for rooms, row in Combined_Data_Frame.iterrows():
     check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['total_rooms'])
-
     if check is not None:
         CorruptRooms = row['total_rooms']
         randRoom = np.random.randint(1000, 2000)
-        CombinedDataFrame.replace(CorruptRooms, randRoom, True)
+        Combined_Data_Frame.replace(CorruptRooms, randRoom, True)
     else:
         pass
-
-for bedrooms, row in CombinedDataFrame.iterrows():
+for bedrooms, row in Combined_Data_Frame.iterrows():
     check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['total_bedrooms'])
-
     if check is not None:
         CorruptBedroom = row['total_bedrooms']
         randBedroom = np.random.randint(1000, 2000)
-        CombinedDataFrame.replace(CorruptBedroom, randBedroom, True)
+        Combined_Data_Frame.replace(CorruptBedroom, randBedroom, True)
     else:
         pass
-
-
-for population, row in CombinedDataFrame.iterrows():
+for population, row in Combined_Data_Frame.iterrows():
     check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['population'])
-
     if check is not None:
         CorruptPopulation = row['population']
         randPopulation = np.random.randint(5000, 10000)
-        CombinedDataFrame.replace(CorruptPopulation, randPopulation, True)
+        Combined_Data_Frame.replace(CorruptPopulation, randPopulation, True)
     else:
         pass
-
-for households, row in CombinedDataFrame.iterrows():
+for households, row in Combined_Data_Frame.iterrows():
     check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['households'])
-
     if check is not None:
         CorruptHouseholds = row['households']
         randHouseholds = np.random.randint(500, 2500)
-        CombinedDataFrame.replace(CorruptHouseholds, randHouseholds, True)
+        Combined_Data_Frame.replace(CorruptHouseholds, randHouseholds, True)
     else:
         pass
-
-for value, row in CombinedDataFrame.iterrows():
+for value, row in Combined_Data_Frame.iterrows():
     check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['median_house_value'])
-
     if check is not None:
         CorruptValue = row['median_house_value']
         randValue = np.random.randint(100000, 250000)
-        CombinedDataFrame.replace(CorruptValue, randValue, True)
+        Combined_Data_Frame.replace(CorruptValue, randValue, True)
     else:
         pass
-
-for income, row in CombinedDataFrame.iterrows():
+for income, row in Combined_Data_Frame.iterrows():
     check = re.search("[A-Za-z][A-Za-z][A-Za-z][A-Za-z]", row['median_income'])
-
     if check is not None:
         CorruptIncome = row['median_income']
         randIncome = np.random.randint(100000, 750000)
-        CombinedDataFrame.replace(CorruptIncome, randIncome, True)
+        Combined_Data_Frame.replace(CorruptIncome, randIncome, True)
     else:
         pass
-
 # connecting to SQL to access housing_project database
 try:
     myConnection = pymysql.connect(host=hostname,
@@ -161,26 +133,20 @@ except Exception as e:
     exit()
 # Printing output for the verification of import
 print("Beginning import")
-
 # Insert DataFrame records one by one
 cursor = myConnection.cursor()
-ColumnList = "`,`".join([str(i) for i in CombinedDataFrame.columns.tolist()])
-
-for i, row in CombinedDataFrame.iterrows():
+ColumnList = "`,`".join([str(i) for i in Combined_Data_Frame.columns.tolist()])
+for i, row in Combined_Data_Frame.iterrows():
     sql = "INSERT INTO `housing` (`" + ColumnList + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
     cursor.execute(sql, tuple(row))
-
     myConnection.commit()
-
 # Collecting total records imported into the database.
 # Because I joined the three files, there is only one cleaning process after which files are then imported.
-
 sqlCount = "SELECT COUNT(*) FROM `housing`"
 cursor.execute(sqlCount)
 CountResult = cursor.fetchall()
 for result in CountResult:
     TotalRecordsImported = result['COUNT(*)']
-
 # Printing output for the verification of cleaned import
 print("Cleaning Housing File data")
 print(f"{TotalRecordsImported} records imported into the database")
@@ -190,12 +156,9 @@ print("Cleaning ZIP File data")
 print(f"{TotalRecordsImported} records imported into the database")
 print("Import completed")
 print()
-
 # Create variables to hold input. Using input to give correct output based on records in database.
-
 print("Beginning validation")
 print()
-
 Valid = False
 while Valid is False:
     askForRooms = input("Total Rooms: {Enter any integer to know more about rooms} ")
@@ -220,7 +183,6 @@ while Valid is False:
     except Exception as e:
         print(f"Please try again.")
         print(f"\t{e}")
-
 Question = False
 while Question is False:
     askForZipCode = input("ZIP Code: {Enter any five digit integer to know more about that zip code} ")
@@ -249,8 +211,6 @@ while Question is False:
     except Exception as e:
         print(f"Please try again.")
         print(f"\t{e}")
-
 # Closing sql
 myConnection.close()
-
 print("Program exiting.")
